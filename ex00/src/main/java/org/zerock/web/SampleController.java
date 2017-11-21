bff949e94e7d306c6b9b0083abeaefb976c909cd
alice965 <alice965@naver.com>
2017-11-21 14:41:05 +0900

    마이바티스 설정등 테스트

---
+++ ex00/src/main/java/org/zerock/web/SampleController.java
@@ -0,0 +1,57 @@
+package org.zerock.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.ProductVO;
+
+@Controller
+public class SampleController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+	
+	@RequestMapping("doA")
+	public void doA() {
+		logger.info("doA Called............");
+	}
+	
+	@RequestMapping("doB")
+	public void doB() {
+		logger.info("doB Called............");
+	}
+	
+	@RequestMapping("doC")
+	public String doC(@ModelAttribute("msg") String msg) {
+		logger.info("doC Called............");
+		
+		return "result";
+	}
+	
+	@RequestMapping("doD")
+	public String doD(Model model) {
+		ProductVO product = new ProductVO("SampleP", 10000);
+		logger.info("doD");
+		model.addAttribute(product);
+		return "productDetail";
+	}
+	@RequestMapping("doE")
+	public String doE(RedirectAttributes rttr) {
+		logger.info("doE called but redirected to doF");
+		rttr.addFlashAttribute("msg", "이것은 리다이렉트된 메시지");
+		return "redirect:/doF";
+	}
+	@RequestMapping("doF")
+	public void doF(@ModelAttribute("msg") String msg) {
+		logger.info("doF Called...." + msg);
+	}
+	
+}
